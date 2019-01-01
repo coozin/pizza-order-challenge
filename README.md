b19dbf0a126eaf092d4683b1d5d9c66eda73b7d8
Lucy Mussida <Lucy@Lucys-MacBook-Pro.local>
2019-01-01 15:34:46 +0100

    added graphQL

--- README.md
+++ README.md
@@ -1,25 +1,9 @@
-# ReduxSimpleStarter
-
-Interested in learning [Redux](https://www.udemy.com/react-redux/)?
-
-### Getting Started
-
-There are two methods for getting started with this repo.
-
-#### Familiar with Git?
-Checkout this repo, install dependencies, then start the gulp process with the following:
+# PizzaPlan
 
 ```
-> git clone https://github.com/StephenGrider/ReduxSimpleStarter.git
-> cd ReduxSimpleStarter
+> git clone https://github.com/coozin/pizza-order-challenge.git
+> cd PizzaPlan
 > npm install
 > npm start
 ```
 
-#### Not Familiar with Git?
-Click [here](https://github.com/StephenGrider/ReactStarter/releases) then download the .zip file.  Extract the contents of the zip file, then open your terminal, change to the project directory, and:
-
-```
-> npm install
-> npm start
-```
--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
 
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+
+const client = new ApolloClient({
+  uri: "https://core-graphql.dev.waldo.photos/pizza"
+});
+
+client
+  .query({
+    query: gql`
+    {
+      pizzaSizes {
+        toppings {
+          topping {
+            name
+            price
+          }
+          defaultSelected
+        }
+        maxToppings
+        name
+        basePrice
+      }
+    }
+    `
+  })
+  .then(result => console.log(result));
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <ApolloProvider client={client}>
+        <div>
+          <h2>My first Apollo app 🚀</h2>
+        </div>
+      </ApolloProvider>
     );
   }
 }
